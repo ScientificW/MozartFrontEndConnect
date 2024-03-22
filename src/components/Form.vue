37d699170842dbe42812147cfbd8b4e8318a9577
Hananoisakura <122717517+Hananoisakura@users.noreply.github.com>
2024-03-22 23:58:55 +0800

    忘了把测试代码改回来

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -125,7 +125,7 @@ const handleClick = async () => {
   
   isLoading.value = true;
   //临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用
-  emit('update:modelValue', true) 
+  // emit('update:modelValue', true) 
 
 // 将表单数据整合进formData
   const formData = new FormData();
@@ -167,7 +167,7 @@ const handleClick = async () => {
       store.commit('setMusic', music.value);
       // 把Exhibition中isSubmitted改成true,实现正常返回后组件变换为Music
       //临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用临时测试用
-      // emit('update:modelValue', true) 
+      emit('update:modelValue', true) 
     }
    
     } catch (error) {
